425a1185a105365d9abcec84cf2b7581ecbe1333
Dhruv Savani <savanidp456@gmail.com>
2024-11-20 08:47:46 -0500

    Updating connection to new phoAdmin Database

--- backend/connection.php
+++ backend/connection.php
@@ -9,18 +9,4 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
    } 
-//  echo "Connected successfully";
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $database= "farm_marketplace";
- 
-// // Create connection
-// $db = mysqli_connect($servername, $username, $password, $database);
- 
-// // Check connection
-// if ($db->connect_error) {
-//   die("Connection failed: " . $db->connect_error);
-// }
-// echo "Connected successfully";
 ?>
