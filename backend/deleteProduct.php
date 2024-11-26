93930514964ea125e79bd1247e3b135b16736baf
Dhruv Savani <savanidp456@gmail.com>
2024-11-25 21:53:22 -0500

    Add deleted orders to Trash Table with limited fields

--- backend/deleteProduct.php
+++ backend/deleteProduct.php
@@ -5,6 +5,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
 
+//Adding Deleted Order into Other Trash Table with only limited fields
 
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
 
